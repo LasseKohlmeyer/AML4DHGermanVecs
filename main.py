0ebb5c9e3a18928bb94092c0afcf9a4bb490db44
Lasse Kohlmeyer <l.kohlmeyer@gmx.net>
2020-04-27 18:45:21 +0200

    experimented with gensim

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from typing import List
 import spacy
-from gensim.models import Word2Vec, Phrases
-
+from gensim import models as gensim
+from gensim.models import Word2Vec, FastText, Phrases
+from gensim.test.utils import get_tmpfile
+from gensim.test.utils import datapath
 
 def tokens_from_file(path: str) -> List[str]:
     with open(path, encoding="utf-8") as f:
@@ -10,9 +12,9 @@ def tokens_from_file(path: str) -> List[str]:
     return data.split("\n")
 
 
-def preprocess(tokens: List[str], lemmatize: bool = True, lower: bool = True,
+def preprocess(tokens: List[str], lemmatize: bool = True, lower: bool = False,
                pos_filter: list = None, remove_stopwords: bool = True,
-               remove_punctuation: bool = True, lan_model = None) -> List[str]:
+               remove_punctuation: bool = False, lan_model=None) -> List[str]:
     def token_representation(token):
         representation = str(token.lemma_) if lemmatize else str(token)
         if lower:
@@ -36,20 +38,34 @@ def preprocess(tokens: List[str], lemmatize: bool = True, lower: bool = True,
     else:
         for doc in nlp.pipe(tokens, disable=['parser', 'ner']):
             for token in doc:
-                if (not remove_stopwords or not token.is_stop) and (not remove_punctuation or token.is_alpha) and token.pos_ in pos_filter:
+                if (not remove_stopwords or not token.is_stop) and (
+                        not remove_punctuation or token.is_alpha) and token.pos_ in pos_filter:
                     preprocessed_tokens.append(token_representation(token))
     return preprocessed_tokens
 
 
-def to_vecs(tokens = List[str], use_phrases: bool = False) -> List[float]:
+def to_vecs(tokens=List[str], use_phrases: bool = False, w2v_model=gensim.Word2Vec) -> gensim.KeyedVectors:
     if use_phrases:
         bigram_transformer = Phrases(tokens)
-        model = Word2Vec(bigram_transformer[tokens], size=100, window=10, min_count=1, workers=4)
+        model = w2v_model(bigram_transformer[tokens], size=100, window=10, min_count=1, workers=4)
     else:
-        model = Word2Vec(tokens, size=300, window=5, min_count=1, workers=4, iter=15)
-    print(model.wv.most_similar("tumor"))
+        model = Word2Vec(tokens, size=300, window=5, min_count=1, workers=4, iter=10)
     return model.wv
 
-tokens = preprocess(tokens_from_file(path="E:/CPG-AMIA2020/Plain Text/cpg-tokens.txt")[:])
+
+def save_vecs(word_vectors: gensim.KeyedVectors, path="E:/AML4DHGermanVecs/test_vecs.kv"):
+    word_vectors.save(get_tmpfile(path))
+
+
+def load_vecs(path="E:/AML4DHGermanVecs/test_vecs.kv") -> gensim.KeyedVectors:
+    return gensim.KeyedVectors.load(path)
+
+
+tokens = preprocess(tokens_from_file(path="E:/CPG-AMIA2020/Plain Text/cpg-tokens.txt")[:1000])
 print(tokens[:10])
-to_vecs([tokens], use_phrases=True)
+vecs = to_vecs([tokens], use_phrases=True)
+print(vecs.most_similar("Tumor"))
+save_vecs(vecs)
+
+# vecs = load_vecs()
+# print(vecs.most_similar("Tumor"))

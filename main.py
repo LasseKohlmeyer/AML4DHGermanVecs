0089908198f804b409d5f268b38644325bf14c5a
Lasse Kohlmeyer <l.kohlmeyer@gmx.net>
2020-05-11 20:33:06 +0200

    added sentence based support

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ from simstring.feature_extractor.word_ngram import WordNgramFeatureExtractor
 from simstring.measure.cosine import CosineMeasure
 from simstring.database.dict import DictDatabase
 from simstring.searcher import Searcher
+from tqdm import tqdm
 from whatlies.embedding import Embedding
 from whatlies.embeddingset import EmbeddingSet
+from itertools import chain
 
 
 class UMLSMapper:
@@ -65,6 +67,20 @@ class UMLSMapper:
                 standardized_tokens.append(token)
         return standardized_tokens
 
+    def standardize_documents(self, documents: List[List[str]]):
+        concept_dict = self.search_all_term_sims(list(chain(*documents)))
+        standardized_documents = []
+        for document in tqdm(documents):
+            standardized_tokens = []
+            for token in document:
+                mapping = concept_dict.get(token)
+                if mapping:
+                    standardized_tokens.append(mapping[0])
+                else:
+                    standardized_tokens.append(token)
+                standardized_documents.append(standardized_tokens)
+        return standardized_documents
+
     def get_umls_vectors_only(self, vectors: gensim.KeyedVectors):
         medical_concepts = [word for word in vectors.index2word if word in self.umls_dict.values()]
         print(len(medical_concepts), medical_concepts[:10])
@@ -84,30 +100,34 @@ class UMLSMapper:
     def replace_UMLS(self, tokens: List[str]) -> List[str]:
         return [self.un_umls(token) for token in tokens if self.un_umls(token)]
 
-    def replace_with_UMLS(self, tokens: List[str], delete_non_umls=False) -> List[str]:
-        def umls_code(token):
-            umls_code = self.umls_dict.get(token)
-            if umls_code is None:
-                if delete_non_umls:
-                    return None
-                else:
-                    return token
+    def umls_code(self, token, delete_non_umls):
+        umls_code = self.umls_dict.get(token)
+        if umls_code is None:
+            if delete_non_umls:
+                return None
             else:
-                return umls_code
+                return token
+        else:
+            return umls_code
 
-        return [umls_code(token) for token in tokens if umls_code(token)]
+    def replace_with_UMLS(self, tokens: List[str], delete_non_umls=False) -> List[str]:
+        return [self.umls_code(token, delete_non_umls) for token in tokens if self.umls_code(token, delete_non_umls)]
+
+    def replace_documents_with_UMLS(self, documents: List[List[str]], delete_non_umls=False) -> List[List[str]]:
+        return [[self.umls_code(token, delete_non_umls) for token in tokens if self.umls_code(token, delete_non_umls)]
+                for tokens in documents]
 
 
-def tokens_from_file(path: str) -> List[str]:
+def lines_from_file(path: str) -> List[str]:
     with open(path, encoding="utf-8") as f:
         data = f.read()
 
     return data.split("\n")
 
 
-def preprocess(tokens: List[str], lemmatize: bool = True, lower: bool = False,
-               pos_filter: list = None, remove_stopwords: bool = True,
-               remove_punctuation: bool = False, lan_model=None) -> List[str]:
+def preprocess(tokens: List[str] = None, documents: List[List[str]] = None, lemmatize: bool = False, lower: bool = False,
+               pos_filter: list = None, remove_stopwords: bool = False,
+               remove_punctuation: bool = False, lan_model=None) -> List[List[str]]:
     def token_representation(token):
         representation = str(token.lemma_) if lemmatize else str(token)
         if lower:
@@ -117,35 +137,63 @@ def preprocess(tokens: List[str], lemmatize: bool = True, lower: bool = False,
     nlp = spacy.load("de_core_news_sm") if lan_model is None else lan_model
     nlp.Defaults.stop_words |= {"der", "die", "das", "Der", "Die", "Das", "bei", "Bei", "In", "in"}
 
-    for word in nlp.Defaults.stop_words:
-        lex = nlp.vocab[word]
-        lex.is_stop = True
-    preprocessed_tokens = []
+    if tokens:
+        for word in nlp.Defaults.stop_words:
+            lex = nlp.vocab[word]
+            lex.is_stop = True
+        preprocessed_tokens = []
 
-    if pos_filter is None:
-        for doc in nlp.pipe(tokens, disable=['parser', 'ner', 'tagger']):
-            for token in doc:
-                if (not remove_stopwords or not token.is_stop) and (not remove_punctuation or token.is_alpha):
-                    preprocessed_tokens.append(token_representation(token))
+        if pos_filter is None:
+            for doc in nlp.pipe(tokens, disable=['parser', 'ner', 'tagger']):
+                for token in doc:
+                    if (not remove_stopwords or not token.is_stop) and (not remove_punctuation or token.is_alpha):
+                        preprocessed_tokens.append(token_representation(token))
 
-    else:
-        for doc in nlp.pipe(tokens, disable=['parser', 'ner']):
-            for token in doc:
-                if (not remove_stopwords or not token.is_stop) and (
-                        not remove_punctuation or token.is_alpha) and token.pos_ in pos_filter:
-                    preprocessed_tokens.append(token_representation(token))
-    return preprocessed_tokens
+        else:
+            for doc in nlp.pipe(tokens, disable=['parser', 'ner']):
+                for token in doc:
+                    if (not remove_stopwords or not token.is_stop) and (
+                            not remove_punctuation or token.is_alpha) and token.pos_ in pos_filter:
+                        preprocessed_tokens.append(token_representation(token))
+        return preprocessed_tokens
+
+    if documents:
+        documents = [' '.join(doc) for doc in documents]
+        for word in nlp.Defaults.stop_words:
+            lex = nlp.vocab[word]
+            lex.is_stop = True
+        preprocessed_documents = []
+
+        if pos_filter is None:
+            for doc in nlp.pipe(documents, disable=['parser', 'ner', 'tagger']):
+                preprocessed_document = []
+                for token in doc:
+                    if (not remove_stopwords or not token.is_stop) and (not remove_punctuation or token.is_alpha):
+                        preprocessed_document.append(token_representation(token))
+                preprocessed_documents.append(preprocessed_document)
+
+        else:
+            for doc in nlp.pipe(documents, disable=['parser', 'ner']):
+                preprocessed_document = []
+                for token in doc:
+                    if (not remove_stopwords or not token.is_stop) and (
+                            not remove_punctuation or token.is_alpha) and token.pos_ in pos_filter:
+                        preprocessed_document.append(token_representation(token))
+                preprocessed_documents.append(preprocessed_document)
+
+        return preprocessed_documents
 
 
 class Embeddings:
     @staticmethod
     def calculate_vectors(tokens=List[str], use_phrases: bool = False,
-                          w2v_model=gensim.Word2Vec) -> gensim.KeyedVectors:
+                          w2v_model=gensim.Word2Vec, dim: int = 300, window=10) -> gensim.KeyedVectors:
+        seed = 42
         if use_phrases:
             bigram_transformer = Phrases(tokens)
-            model = w2v_model(bigram_transformer[tokens], size=100, window=10, min_count=1, workers=4)
+            model = w2v_model(bigram_transformer[tokens], size=dim, window=window, min_count=1, workers=4, seed=seed)
         else:
-            model = Word2Vec(tokens, size=300, window=5, min_count=1, workers=4, iter=15)
+            model = Word2Vec(tokens, size=dim, window=window, min_count=1, workers=4, iter=15, seed=seed)
         return model.wv
 
     @staticmethod
@@ -184,32 +232,54 @@ class Embeddings:
 
 umls_mapper = UMLSMapper(from_file='E:/AML4DH-DATA/UMLS/GER_MRCONSO.RRF')
 
-# cpg_words = tokens_from_file(path="E:/AML4DH-DATA/CPG-AMIA2020/Plain Text/cpg-tokens.txt")
-#
-# # Preprocessing
+# load sentences
+cpq_sentences = lines_from_file(path="E:/AML4DH-DATA/CPG-AMIA2020/Plain Text/cpg-sentences.txt")
+
+# tokenization
+cpq_sentences = [sentence.split() for sentence in cpq_sentences]
+
+cpg_words = lines_from_file(path="E:/AML4DH-DATA/CPG-AMIA2020/Plain Text/cpg-tokens.txt")
+
+# Preprocessing
 # cpg_words = umls_mapper.standardize_words(cpg_words)
 # cpg_words = umls_mapper.replace_with_UMLS(cpg_words)
 # cpg_words = preprocess(cpg_words)
 # print((cpg_words[:100]))
+
+# cpq_sentences = umls_mapper.standardize_documents(cpq_sentences)
+cpq_sentences = umls_mapper.replace_documents_with_UMLS(cpq_sentences)
+cpq_sentences = preprocess(documents=cpq_sentences, lemmatize=True, remove_stopwords=True)
+
+print((cpq_sentences[:10]))
+
+
 #
 # # Vectorization
-# vecs = Embeddings.calculate_vectors([cpg_words], use_phrases=True)
-#
-#
+# vecs = Embeddings.calculate_vectors([cpg_words], use_phrases=False)
+vecs = Embeddings.calculate_vectors(cpq_sentences, use_phrases=False)
+
+
 # Embeddings.save(vecs)
 #
 # vecs = Embeddings.load()
-# # Evaluation
-# for c, v in vecs.most_similar(umls_mapper.umls_dict["Zervix"]):
+# print(vecs.vocab)
+
+# Evaluation
+for c, v in vecs.most_similar("Cisplatin"):
+    print(umls_mapper.un_umls(c), v)
+concept_vecs = umls_mapper.get_umls_vectors_only(vecs)
+
+
+# for c, v in vecs.most_similar(umls_mapper.umls_dict["Cisplatin"]):
 #     print(umls_mapper.un_umls(c), v)
 # concept_vecs = umls_mapper.get_umls_vectors_only(vecs)
-#
-#
-# Embeddings.restrict_vectors(vecs, concept_vecs.keys())
-# Embeddings.save(vecs, path="E:/AML4DHGermanVecs/test_vecs_1.kv")
+
+
+Embeddings.restrict_vectors(vecs, concept_vecs.keys())
+Embeddings.save(vecs, path="E:/AML4DHGermanVecs/test_vecs_1.kv")
 
 vecs = Embeddings.load(path="E:/AML4DHGermanVecs/test_vecs_1.kv")
-for c, v in vecs.most_similar(umls_mapper.umls_dict["Zervix"]):
+for c, v in vecs.most_similar(umls_mapper.umls_dict["Cisplatin"]):
     print(umls_mapper.un_umls(c, single_return=True), v)
 
 # print([(umls_mapper.un_umls(c), Embedding(umls_mapper.un_umls(c), vecs[c])) for c in vecs.vocab])

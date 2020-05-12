a9e06001614b875bd43a8b66aee2b516024dc91e
Lasse Kohlmeyer <l.kohlmeyer@gmx.net>
2020-05-12 19:59:53 +0200

    experimented with benchmark

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from typing import List, Dict, Iterable
 import spacy
+import os
 import numpy as np
 from gensim import models as gensim
 from gensim.models import Word2Vec, FastText, Phrases
@@ -19,20 +20,22 @@ from itertools import chain
 
 class UMLSMapper:
     # https://www.ncbi.nlm.nih.gov/books/NBK9685/table/ch03.T.concept_names_and_sources_file_mr/
-    def __init__(self, from_file=None, umls_words: Iterable[str] = None):
+    def __init__(self, from_dir=None, umls_words: Iterable[str] = None):
         # self.db = DictDatabase(WordNgramFeatureExtractor(2))
         self.db = DictDatabase(CharacterNgramFeatureExtractor(2))
 
-        if from_file:
-            self.umls_dict, self.umls_reverse_dict = self.load_UMLS_dict(path=from_file)
+        if from_dir:
+            self.umls_dict, self.umls_reverse_dict = self.load_umls_dict(directory=from_dir)
             self.add_words_to_db(self.umls_dict.keys())
         else:
             self.add_words_to_db(umls_words)
 
-    def load_UMLS_dict(self, path: str = 'E:/AML4DH-DATA/UMLS/GER_MRCONSO.RRF'):
+    def load_umls_dict(self, directory: str):
+        path = os.path.join(directory, "GER_MRCONSO.RRF")
         df = pd.read_csv(path, delimiter="|", header=None)
         df.columns = ["CUI", "LAT", "TS", "LUI", "STT", "SUI", "ISPREF", "AUI", "SAUI", "SCUI", "SDUI", "SAB", "TTY",
                       "CODE", "STR", "SRL", "SUPPRESS", "CVF", "NONE"]
+        df = df.drop(columns=['NONE'])
         dic = {row["STR"]: row["CUI"] for id, row in df.iterrows()}
         rev_dic = defaultdict(list)
         for key, value in dic.items():
@@ -97,7 +100,7 @@ class UMLSMapper:
         else:
             return res
 
-    def replace_UMLS(self, tokens: List[str]) -> List[str]:
+    def replace_umls(self, tokens: List[str]) -> List[str]:
         return [self.un_umls(token) for token in tokens if self.un_umls(token)]
 
     def umls_code(self, token, delete_non_umls):
@@ -116,6 +119,27 @@ class UMLSMapper:
     def replace_documents_with_UMLS(self, documents: List[List[str]], delete_non_umls=False) -> List[List[str]]:
         return [[self.umls_code(token, delete_non_umls) for token in tokens if self.umls_code(token, delete_non_umls)]
                 for tokens in documents]
+
+
+class UMLSEvaluator:
+    def __init__(self, vectors: gensim.KeyedVectors, from_dir="E:/AML4DH-DATA/UMLS"):
+        self.vocab = vectors.vocab
+        self.concept2category, self.category2concepts = self.load_umls_semantics(directory=from_dir)
+
+    def load_umls_semantics(self, directory):
+        path = os.path.join(directory, "MRSTY.RRF")
+        df = pd.read_csv(path, delimiter="|", header=None)
+        df.columns = ["CUI", "TUI", "STN", "STY", "ATUI", "CVF", "NONE"]
+        df = df.drop(columns=['NONE'])
+
+        concept2category = defaultdict(list)
+        category2concepts = defaultdict(list)
+
+        for i, row in df.iterrows():
+            if row["CUI"] in self.vocab:
+                concept2category[row["CUI"]].append(row["STY"])
+                category2concepts[row["STY"]].append(row["CUI"])
+        return concept2category, category2concepts
 
 
 def lines_from_file(path: str) -> List[str]:
@@ -230,60 +254,156 @@ class Embeddings:
         return gensim.KeyedVectors.load(path)
 
 
-umls_mapper = UMLSMapper(from_file='E:/AML4DH-DATA/UMLS/GER_MRCONSO.RRF')
+umls_mapper = UMLSMapper(from_dir='E:/AML4DH-DATA/UMLS')
+
+
 
 # load sentences
-cpq_sentences = lines_from_file(path="E:/AML4DH-DATA/CPG-AMIA2020/Plain Text/cpg-sentences.txt")
-
-# tokenization
-cpq_sentences = [sentence.split() for sentence in cpq_sentences]
-
-cpg_words = lines_from_file(path="E:/AML4DH-DATA/CPG-AMIA2020/Plain Text/cpg-tokens.txt")
-
-# Preprocessing
-# cpg_words = umls_mapper.standardize_words(cpg_words)
-# cpg_words = umls_mapper.replace_with_UMLS(cpg_words)
-# cpg_words = preprocess(cpg_words)
-# print((cpg_words[:100]))
-
-# cpq_sentences = umls_mapper.standardize_documents(cpq_sentences)
-cpq_sentences = umls_mapper.replace_documents_with_UMLS(cpq_sentences)
-cpq_sentences = preprocess(documents=cpq_sentences, lemmatize=True, remove_stopwords=True)
-
-print((cpq_sentences[:10]))
-
-
+# cpq_sentences = lines_from_file(path="E:/AML4DH-DATA/CPG-AMIA2020/Plain Text/cpg-sentences.txt")
 #
-# # Vectorization
-# vecs = Embeddings.calculate_vectors([cpg_words], use_phrases=False)
-vecs = Embeddings.calculate_vectors(cpq_sentences, use_phrases=False)
-
-
-# Embeddings.save(vecs)
+# # tokenization
+# cpq_sentences = [sentence.split() for sentence in cpq_sentences]
 #
-# vecs = Embeddings.load()
-# print(vecs.vocab)
-
-# Evaluation
-for c, v in vecs.most_similar("Cisplatin"):
-    print(umls_mapper.un_umls(c), v)
-concept_vecs = umls_mapper.get_umls_vectors_only(vecs)
-
-
-# for c, v in vecs.most_similar(umls_mapper.umls_dict["Cisplatin"]):
+# cpg_words = lines_from_file(path="E:/AML4DH-DATA/CPG-AMIA2020/Plain Text/cpg-tokens.txt")
+#
+# # Preprocessing
+# # cpg_words = umls_mapper.standardize_words(cpg_words)
+# # cpg_words = umls_mapper.replace_with_UMLS(cpg_words)
+# # cpg_words = preprocess(cpg_words)
+# # print((cpg_words[:100]))
+#
+# # cpq_sentences = umls_mapper.standardize_documents(cpq_sentences)
+# cpq_sentences = umls_mapper.replace_documents_with_UMLS(cpq_sentences)
+# cpq_sentences = preprocess(documents=cpq_sentences, lemmatize=True, remove_stopwords=True)
+#
+# print((cpq_sentences[:10]))
+#
+#
+# #
+# # # Vectorization
+# # vecs = Embeddings.calculate_vectors([cpg_words], use_phrases=False)
+# vecs = Embeddings.calculate_vectors(cpq_sentences, use_phrases=False)
+#
+#
+# # Embeddings.save(vecs)
+# #
+# # vecs = Embeddings.load()
+# # print(vecs.vocab)
+#
+# # Evaluation
+# for c, v in vecs.most_similar("Cisplatin"):
 #     print(umls_mapper.un_umls(c), v)
 # concept_vecs = umls_mapper.get_umls_vectors_only(vecs)
-
-
-Embeddings.restrict_vectors(vecs, concept_vecs.keys())
-Embeddings.save(vecs, path="E:/AML4DHGermanVecs/test_vecs_1.kv")
+#
+#
+# # for c, v in vecs.most_similar(umls_mapper.umls_dict["Cisplatin"]):
+# #     print(umls_mapper.un_umls(c), v)
+# # concept_vecs = umls_mapper.get_umls_vectors_only(vecs)
+#
+#
+# Embeddings.restrict_vectors(vecs, concept_vecs.keys())
+# Embeddings.save(vecs, path="E:/AML4DHGermanVecs/test_vecs_1.kv")
 
 vecs = Embeddings.load(path="E:/AML4DHGermanVecs/test_vecs_1.kv")
-for c, v in vecs.most_similar(umls_mapper.umls_dict["Cisplatin"]):
+
+def analogies(vectors, start, minus, plus, umls: UMLSMapper):
+    if umls:
+        return vectors.most_similar(positive=[umls.umls_dict[start], umls.umls_dict[plus]], negative=[umls.umls_dict[minus]])
+    else:
+        return vectors.most_similar(positive=[start, plus], negative=[minus])
+
+def similarities(vectors, word, umls):
+    if umls:
+        return vectors.most_similar(umls.umls_dict[word])
+    else:
+        return vectors.most_similar(word)
+
+def similarities2(self, word):
+    if self.umls:
+        return self.most_similar(self.umls.umls_dict[word])
+    else:
+        return self.most_similar(word)
+
+vecs.umls = umls_mapper
+gensim.KeyedVectors.monkey_sims = similarities2
+print(vecs.monkey_sims("Asthma"))
+for c, v in analogies(vecs, "Asthma", "Lunge", "Herz", umls=umls_mapper):
+    print(umls_mapper.un_umls(c, single_return=True), v)
+
+
+for c, v in similarities(vecs, "Hepatitis", umls=umls_mapper):
+    print(umls_mapper.un_umls(c, single_return=True), v)
+
+for c, v in similarities(vecs, "Cisplatin", umls=umls_mapper):
     print(umls_mapper.un_umls(c, single_return=True), v)
 
 # print([(umls_mapper.un_umls(c), Embedding(umls_mapper.un_umls(c), vecs[c])) for c in vecs.vocab])
+def pairwise_cosine(concepts1, concepts2=None):
+    def cosine(word1=None, word2=None, concept1=None, concept2=None):
+        if word1:
+            return vecs.similarity(umls_mapper.umls_dict[word1], umls_mapper.umls_dict[word2])
+        else:
+            return vecs.similarity(concept1, concept2)
+    if concepts2 is None:
+        concepts2 = concepts1
+        s = 0
+        count = 0
+        for i, concept1 in enumerate(concepts1):
+            for j, concept2 in enumerate(concepts2):
+                if j > i:
+                    c = cosine(concept1=concept1, concept2=concept2)
+                    if c < 0:
+                        c = -c
+                    s += c
+                    count += 1
+        return s / count
+    else:
+        s = 0
+        count = 0
+        for i, concept1 in enumerate(concepts1):
+            for j, concept2 in enumerate(concepts2):
+                c = cosine(concept1=concept1, concept2=concept2)
+                if c < 0:
+                    c = -c
+                s += c
+                count += 1
+        return s / count
+
+
+def bla():
+    choosen_category = "Nucleotide Sequence"
+    other_categories = evaluator.category2concepts.keys()
+    p1 = pairwise_cosine(evaluator.category2concepts[choosen_category])
+    print(p1)
+
+    p2s = []
+    for other_category in other_categories:
+        if other_category == choosen_category:
+            continue
+        choosen_concepts = evaluator.category2concepts[choosen_category]
+        other_concepts = evaluator.category2concepts[other_category]
+        if len(choosen_concepts) == 0 or len(other_concepts) == 0:
+            continue
+        p2 = pairwise_cosine(choosen_concepts, other_concepts)
+        p2s.append(p2)
+
+    avg_p2 = sum(p2s) / len(p2s)
+    print(p2s)
+    print(p1, avg_p2, p1 - avg_p2)
+    return p1 - avg_p2
+
+
+bla()
+
+evaluator = UMLSEvaluator(vectors=vecs)
+p1 = pairwise_cosine(evaluator.category2concepts["Medical Device"])
+print(p1)
+p2 = pairwise_cosine(evaluator.category2concepts["Medical Device"], evaluator.category2concepts["Health Care Related Organization"])
+print(p2)
+
 emb = EmbeddingSet({umls_mapper.un_umls(c, single_return=True): Embedding(umls_mapper.un_umls(c, single_return=True), vecs[c]) for c in vecs.vocab})
 # emb = EmbeddingSet({c: Embedding(c, vecs[c]) for c in vecs.vocab})
 
 emb.plot_interactive("Fibroblasten","Fremdkörper")
+
+# replace multi words

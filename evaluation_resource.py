de8d2c8197639cb7c2e60cd0e4552b069a65124d
Lasse Kohlmeyer <l.kohlmeyer@gmx.net>
2020-05-23 14:25:49 +0200

    restructured use of evaluator classes

--- evaluation_resource.py
+++ evaluation_resource.py
@@ -1,14 +1,21 @@
+from abc import ABC, abstractmethod
 import os
 from typing import Dict, List
 from collections import defaultdict
 
 
-class NDFEvaluator:
+class EvaluationResource(ABC):
+    @abstractmethod
+    def load_semantics(self, directory: str):
+        raise NotImplementedError
+
+
+class NDFEvaluator(EvaluationResource):
     def __init__(self, from_dir="E:/AML4DH-DATA/NDF"):
         print("initialize UMLSEvaluator...")
-        self.may_treat, self.may_prevent, self.reverted_treat, self.reverted_prevent = self.load_ndf_semantics(from_dir)
+        self.may_treat, self.may_prevent, self.reverted_treat, self.reverted_prevent = self.load_semantics(from_dir)
 
-    def load_ndf_semantics(self, directory: str):
+    def load_semantics(self, directory: str):
         def load_file(file_name: str) -> Dict[str, List[str]]:
             with open(os.path.join(directory, file_name), encoding="utf-8") as f:
                 data = f.read()
@@ -28,13 +35,9 @@ class NDFEvaluator:
                     reverted_dict[value].append(key)
             return reverted_dict
 
-
         may_treat_dict = load_file(file_name="may_treat_cui.txt")
         may_prevent_dict = load_file(file_name="may_prevent_cui.txt")
 
         may_treat_reverted_dict = revert(may_treat_dict)
         may_prevent_reverted_dict = revert(may_treat_dict)
         return may_treat_dict, may_prevent_dict, may_treat_reverted_dict, may_prevent_reverted_dict
-
-
-eval = NDFEvaluator()

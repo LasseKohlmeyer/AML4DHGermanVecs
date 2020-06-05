a119a0205c33ad7aefba9ab4d1022b1902d5573d
Lasse Kohlmeyer <l.kohlmeyer@gmx.net>
2020-06-05 20:46:23 +0200

    added vectroization for new datasets

--- vectorize_data.py
+++ vectorize_data.py
@@ -1,16 +1,11 @@
-from typing import List
+from typing import List, Union
 
+import gensim
 import spacy
 
 from UMLS import UMLSMapper
 from embeddings import Embeddings
-
-
-def lines_from_file(path: str) -> List[str]:
-    with open(path, encoding="utf-8") as f:
-        data = f.read()
-
-    return data.split("\n")
+from transform_data import DataHandler
 
 
 def preprocess(tokens: List[str] = None, documents: List[List[str]] = None, lemmatize: bool = False, lower: bool = False,
@@ -72,11 +67,23 @@ def preprocess(tokens: List[str] = None, documents: List[List[str]] = None, lemm
         return preprocessed_documents
 
 
-def main():
+def sentence_data2vec(path: str, embedding_name: str,
+                      embeddings_algorithm: Union[str, gensim.models.Word2Vec, gensim.models.FastText] = "word2vec",
+                      number_sentences: int = None,
+                      use_phrases: bool = False,
+                      restrict_vectors: bool = False):
+    if isinstance(embeddings_algorithm, str) and embeddings_algorithm.lower() == "word2vec":
+        embeddings_algorithm = gensim.models.Word2Vec
+    if isinstance(embeddings_algorithm, str) and embeddings_algorithm.lower() == "fasttext":
+        embeddings_algorithm = gensim.models.FastText
+    if isinstance(embeddings_algorithm, str) and embeddings_algorithm.lower() == "glove":
+        embeddings_algorithm = Embeddings.glove_vectors
+
     umls_mapper = UMLSMapper(from_dir='E:/AML4DH-DATA/UMLS')
-    # load sentences
-    cpq_sentences = lines_from_file(path="E:/AML4DH-DATA/CPG-AMIA2020/Plain Text/cpg-sentences.txt")
-    print((cpq_sentences[:10]))
+    data_sentences = DataHandler.lines_from_file(path=path)
+    if number_sentences:
+        data_sentences = data_sentences[:number_sentences]
+    print((data_sentences[:10]))
 
     # cpg_words = lines_from_file(path="E:/AML4DH-DATA/CPG-AMIA2020/Plain Text/cpg-tokens.txt")
 
@@ -86,21 +93,86 @@ def main():
     # cpg_words = preprocess(cpg_words)
     # print((cpg_words[:100]))
 
-    # cpq_sentences = umls_mapper.standardize_documents(cpq_sentences)
-    # cpq_sentences = umls_mapper.replace_documents_with_umls(cpq_sentences)
-    cpq_sentences = umls_mapper.replace_documents_with_spacy(cpq_sentences)
-    # cpq_sentences = umls_mapper.replace_documents_with_umls_smart(cpq_sentences)
-    # cpq_sentences = preprocess(documents=cpq_sentences, lemmatize=True, remove_stopwords=True)
-    print((cpq_sentences[:10]))
+    # data_sentences = umls_mapper.standardize_documents(data_sentences)
+    # data_sentences = umls_mapper.replace_documents_with_umls(data_sentences)
+    sents = [data_sentences[20124], data_sentences[20139]]
+    data_sentences = umls_mapper.replace_documents_with_spacy(data_sentences)
+    print(data_sentences[:10])
+    # for s in data_sentences:
+    #     print(s)
+    # data_sentences = umls_mapper.replace_documents_with_umls_smart(data_sentences)
+    # data_sentences = preprocess(documents=data_sentences, lemmatize=True, remove_stopwords=True)
 
 
-    #
-    # # Vectorization
     # vecs = Embeddings.calculate_vectors([cpg_words], use_phrases=False)
-    vecs = Embeddings.calculate_vectors(cpq_sentences, use_phrases=False)
-    file_name = "no_prep_vecs_test"
+    vecs = Embeddings.calculate_vectors(data_sentences, use_phrases=use_phrases, embedding_algorithm=embeddings_algorithm)
+    print(f'Got {len(vecs.vocab)} vectors for {len(data_sentences)} sentences')
 
-    Embeddings.save_medical(vecs, file_name, umls_mapper)
+    Embeddings.save_medical(vecs, embedding_name, umls_mapper, restrict=restrict_vectors)
+
+def main():
+
+    # News
+    # sentence_data2vec(path="E:/AML4DH-DATA/2015_3M_sentences/deu_news_2015_3M-sentences.txt",
+    #                   embedding_name="3M_news",
+    #                   embeddings_algorithm="word2vec",
+    #                   number_sentences=None)
+
+    # sentence_data2vec(path="E:/AML4DH-DATA/2015_3M_sentences/deu_news_2015_3M-sentences.txt",
+    #                   embedding_name="60K_news_fastText",
+    #                   embeddings_algorithm="fastText",
+    #                   number_sentences=60000)
+
+    # GGPONC
+    # sentence_data2vec(path="E:/AML4DH-DATA/CPG-AMIA2020/Plain Text/cpg-sentences.txt",
+    #                   embedding_name="GGPONC",
+    #                   embeddings_algorithm="word2vec")
+
+    # sentence_data2vec(path="E:/AML4DH-DATA/CPG-AMIA2020/Plain Text/cpg-sentences.txt",
+    #                   embedding_name="GGPONC_fastText",
+    #                   embeddings_algorithm="fastText")
+
+    sentence_data2vec(path="E:/AML4DH-DATA/CPG-AMIA2020/Plain Text/cpg-sentences.txt",
+                      embedding_name="GGPONC_glove",
+                      embeddings_algorithm="Glove")
+
+    # JSYNC
+    # sentence_data2vec(path="E:/AML4DH-DATA/JSynCC/jsynncc-sentences.txt",
+    #                   embedding_name="JSynCC",
+    #                   embeddings_algorithm="word2vec")
+
+    # German PubMed
+
+
+    # # load sentences
+    # # https: // www.kaggle.com / rtatman / 3 - million - german - sentences / data?select = deu_news_2015_3M - sentences.txt
+    # data_sentences = lines_from_file(path="E:/AML4DH-DATA/2015_3M_sentences/deu_news_2015_3M-sentences.txt")
+    # # data_sentences = lines_from_file(path="E:/AML4DH-DATA/CPG-AMIA2020/Plain Text/cpg-sentences.txt")
+    # print((data_sentences[:10]))
+    #
+    # # cpg_words = lines_from_file(path="E:/AML4DH-DATA/CPG-AMIA2020/Plain Text/cpg-tokens.txt")
+    #
+    # # Preprocessing
+    # # cpg_words = umls_mapper.standardize_words(cpg_words)
+    # # cpg_words = umls_mapper.replace_with_umls(cpg_words)
+    # # cpg_words = preprocess(cpg_words)
+    # # print((cpg_words[:100]))
+    #
+    # # data_sentences = umls_mapper.standardize_documents(data_sentences)
+    # # data_sentences = umls_mapper.replace_documents_with_umls(data_sentences)
+    # data_sentences = umls_mapper.replace_documents_with_spacy(data_sentences)
+    # # data_sentences = umls_mapper.replace_documents_with_umls_smart(data_sentences)
+    # # data_sentences = preprocess(documents=data_sentences, lemmatize=True, remove_stopwords=True)
+    # print((data_sentences[:10]))
+    #
+    #
+    # #
+    # # # Vectorization
+    # # vecs = Embeddings.calculate_vectors([cpg_words], use_phrases=False)
+    # vecs = Embeddings.calculate_vectors(data_sentences, use_phrases=False)
+    # file_name = "no_prep_vecs_test"
+    #
+    # Embeddings.save_medical(vecs, file_name, umls_mapper)
 
 
 if __name__ == "__main__":

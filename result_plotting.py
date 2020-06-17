52c8a83216b71110c17664b318bf6f1418c733a8
Lasse Kohlmeyer <l.kohlmeyer@gmx.net>
2020-06-17 17:57:51 +0200

    worked on plotting

--- result_plotting.py
+++ result_plotting.py
@@ -3,11 +3,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from typing import List
 
+import matplotlib.pyplot as plt
+from typing import List
 
-def plot_single_benchmark(df: pd.DataFrame, benchmark: str, grouping: str, coloring: str, paper_mode: bool = True):
-    df_copy = df[[grouping, coloring, benchmark]]
+
+def plot_single_benchmark(df: pd.DataFrame, benchmark: str, grouping: List[str], coloring: str,
+                          paper_mode: bool = True):
+    if coloring not in grouping:
+        grouping = [*grouping, coloring]
+
+    df_copy = df[[*grouping, benchmark]]
+
     legend_labels = df_copy[coloring].drop_duplicates().iloc[::-1]
-    df_copy = df_copy.set_index([grouping, coloring])
+    df_copy = df_copy.set_index(grouping)
+
     if paper_mode:
         font_size = 14
         legend_size = 10
@@ -24,9 +33,11 @@ def plot_single_benchmark(df: pd.DataFrame, benchmark: str, grouping: str, color
 
     ax = df_copy.unstack().plot(kind="bar", title=title, grid=grid, alpha=0.85, rot=90)
     #     ax.spines['bottom'].set_position('zero')
-    ax.set_xlabel(grouping, fontsize=font_size)
+
+    #     ax.set_xlabel(grouping, fontsize=font_size)
     ax.set_ylim(ymin=0)
     ax.set_ylabel("Score", fontsize=font_size)
+
     if len(legend_labels) > 1:
         ax.legend(legend_labels, title=coloring, title_fontsize=legend_size, fontsize=legend_size, loc="best")
     plt.show()
@@ -34,7 +45,7 @@ def plot_single_benchmark(df: pd.DataFrame, benchmark: str, grouping: str, color
     # plot_single_benchmark(df, benchmark="HumanAssessment", grouping="Algorithm", stratification="Data set")
 
 
-def plot_counts(df: pd.DataFrame, x_attribute: str, coloring: List[str], paper_mode: bool = True):
+def plot_counts(df: pd.DataFrame, x_attribute: str, grouping: str, coloring: List[str], paper_mode: bool = True):
     columns = [x_attribute]
     columns.extend(coloring)
     legend_labels = coloring
@@ -55,8 +66,11 @@ def plot_counts(df: pd.DataFrame, x_attribute: str, coloring: List[str], paper_m
     count_df = df[columns].drop_duplicates()
     ax = count_df.plot(x_attribute, coloring, kind="bar", alpha=0.75, title=title, grid=grid)
     ax.set_ylabel("Count score")
+
     if len(legend_labels) > 1:
         ax.legend(legend_labels, title="Counts", loc="best", title_fontsize=legend_size, fontsize=legend_size)
+    else:
+        ax.get_legend().remove()
     plt.show()
 
     # plot_counts(df, x_attribute="Data set", stratification=["# Concepts"])
@@ -65,11 +79,4 @@ def plot_counts(df: pd.DataFrame, x_attribute: str, coloring: List[str], paper_m
 def mean_group_df(df: pd.DataFrame, group: str):
     return df.groupby(group).mean()
     # mean_group_df(df, group="Data set")
-
-
-
-
-
-
-
 

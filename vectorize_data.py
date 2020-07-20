7cf092820e776440754eac5752664b6064a250e5
Lasse Kohlmeyer <l.kohlmeyer@gmx.net>
2020-07-20 18:13:44 +0200

    added recent changes

--- vectorize_data.py
+++ vectorize_data.py
@@ -6,11 +6,33 @@ from utils.transform_data import DataHandler, ConfigLoader
 
 def main():
     config = ConfigLoader.get_config()
-    umls_mapper = UMLSMapper(from_dir=config["PATH"]["UMLS"])
-    Embeddings.set_umls_mapper(umls_mapper)
-    # News
+    Embeddings.set_config_and_get_umls_mapper(config)
+    # umls_mapper = UMLSMapper(from_dir=config["PATH"]["UMLS"])
+    # Embeddings.set_umls_mapper(umls_mapper)
+
+
     news_path = os.path.join(config['PATH']['News'], 'news_2015_3M-sentences.txt')
     news_path_julie = os.path.join(config['PATH']['News'], 'news_2015_3M-sentences_JULIE.txt')
+    ggponc_path = os.path.join(config['PATH']['GGPONC'], 'Plain Text', 'cpg-sentences.txt')
+    ggponc_path_julie = os.path.join(config['PATH']['GGPONC'], 'Plain Text', 'cpg-sentences_JULIE.txt')
+    jsynncc_path = os.path.join(config['PATH']['JSynnCC'], 'jsynncc-sentences.txt')
+    jsynncc_path_julie = os.path.join(config['PATH']['JSynnCC'], 'jsynncc-sentences_JULIE.txt')
+    pubmed_path = os.path.join(config['PATH']['PubMed'], 'all_sentences.txt')
+    pubmed_path_julie = os.path.join(config['PATH']['PubMed'], 'all_sentences_JULIE.txt')
+    if not DataHandler.path_exists(pubmed_path):
+        DataHandler.read_files_and_save_sentences_to_dir(config['PATH']['PubMed'])
+    paths = [
+        ggponc_path,
+        jsynncc_path,
+        pubmed_path,
+    ]
+    paths_julie = [
+        ggponc_path_julie,
+        jsynncc_path_julie,
+        pubmed_path_julie,
+    ]
+
+    # News
     # Embeddings.sentence_data2vec(path=news_path,
     #                              embedding_name="3M_news",
     #                              embeddings_algorithm="word2vec")
@@ -72,10 +94,20 @@ def main():
     #                              embeddings_algorithm="Glove",
     #                              number_sentences=100000,
     #                              umls_replacement=False)
+    # Embeddings.sentence_data2vec(path=news_path,
+    #                              embedding_name="3M_news_no_cui",
+    #                              embeddings_algorithm="word2vec",
+    #                              umls_replacement=False)
+    # Embeddings.sentence_data2vec(path=news_path,
+    #                              embedding_name="3M_news_fastText_no_cui",
+    #                              embeddings_algorithm="fastText",
+    #                              umls_replacement=False)
+    # Embeddings.sentence_data2vec(path=news_path,
+    #                              embedding_name="3M_news_Glove_no_cui",
+    #                              embeddings_algorithm="Glove",
+    #                              umls_replacement=False)
 
     # GGPONC
-    ggponc_path = os.path.join(config['PATH']['GGPONC'], 'Plain Text', 'cpg-sentences.txt')
-    ggponc_path_julie = os.path.join(config['PATH']['GGPONC'], 'Plain Text', 'cpg-sentences_JULIE.txt')
     # Embeddings.sentence_data2vec(path=ggponc_path,
     #                              embedding_name="GGPONC",
     #                              embeddings_algorithm="word2vec")
@@ -132,8 +164,6 @@ def main():
     #                              umls_replacement=False)
     #
     # # JSYNNCC
-    jsynncc_path = os.path.join(config['PATH']['JSynnCC'], 'jsynncc-sentences.txt')
-    jsynncc_path_julie = os.path.join(config['PATH']['JSynnCC'], 'jsynncc-sentences_JULIE.txt')
     # Embeddings.sentence_data2vec(path=jsynncc_path,
     #                              embedding_name="JSynnCC",
     #                              embeddings_algorithm="word2vec")
@@ -192,12 +222,7 @@ def main():
 
 
     # German PubMed
-    pubmed_path = os.path.join(config['PATH']['PubMed'], 'all_sentences.txt')
-    pubmed_path_julie = os.path.join(config['PATH']['PubMed'], 'all_sentences_JULIE.txt')
-    if not DataHandler.path_exists(pubmed_path):
-        DataHandler.read_files_and_save_sentences_to_dir(config['PATH']['PubMed'])
-
-    # Embeddings.sentence_data2vec(path=pubmed_path,
+     # Embeddings.sentence_data2vec(path=pubmed_path,
     #                              embedding_name="PubMed",
     #                              embeddings_algorithm="word2vec")
     # Embeddings.sentence_data2vec(path=pubmed_path,
@@ -236,7 +261,6 @@ def main():
     #                              embeddings_algorithm="Glove",
     #                              umls_replacement=False
     #                              )
-    # not calc yet
     # Embeddings.sentence_data2vec(path=pubmed_path_julie,
     #                              embedding_name="PubMed_JULIE",
     #                              embeddings_algorithm="word2vec",
@@ -254,11 +278,6 @@ def main():
     #                              )
 
     # Medical Concat
-    # paths = [
-    #     ggponc_path,
-    #     jsynncc_path,
-    #     pubmed_path,
-    # ]
     # Embeddings.sentence_data2vec(path=paths,
     #                              embedding_name="German_Medical",
     #                              embeddings_algorithm="word2vec",
@@ -319,26 +338,23 @@ def main():
     #                              embeddings_algorithm="Glove",
     #                              umls_replacement=False
     #                              )
-    # paths = [
-    #     ggponc_path_julie,
-    #     jsynncc_path_julie,
-    #     pubmed_path_julie,
-    # ]
-    # Embeddings.sentence_data2vec(path=paths,
+
+    # Embeddings.sentence_data2vec(path=paths_julie,
     #                              embedding_name="German_Medical_JULIE",
     #                              embeddings_algorithm="word2vec",
     #                              umls_replacement=False
     #                              )
-    # Embeddings.sentence_data2vec(path=paths,
+    # Embeddings.sentence_data2vec(path=paths_julie,
     #                              embedding_name="German_Medical_fastText_JULIE",
     #                              embeddings_algorithm="fastText",
     #                              umls_replacement=False
     #                              )
-    # Embeddings.sentence_data2vec(path=paths,
-    #                              embedding_name="German_Medical_Glove_JULIE",
-    #                              embeddings_algorithm="Glove",
-    #                              umls_replacement=False
-    #                              )
+    Embeddings.sentence_data2vec(path=paths_julie,
+                                 embedding_name="German_Medical_Glove_JULIE_P",
+                                 embeddings_algorithm="Glove",
+                                 umls_replacement=False,
+                                 restrict_vectors=True
+                                 )
     # Flair
     # Embeddings.sentence_data2vec(path=ggponc_path,
     #                              embedding_name="Flair",
